2867c814e79503fd2a1e3284ab0fb226e7064003
Andrei-Mihai Nicolae <andrei@nicolaeandrei.com>
2026-02-24 18:12:46 +0200

    fix radiuses for max phones

--- src/compose.rs
+++ src/compose.rs
@@ -110,8 +110,8 @@ pub fn compose_scene(
         let ratio = match phone.model {
             Some(PhoneModel::Iphone16Pro) => 0.16,
             Some(PhoneModel::Iphone17Pro) => 0.145,
-            Some(PhoneModel::Iphone16ProMax) => 0.135,
-            Some(PhoneModel::Iphone17ProMax) => 0.13,
+            Some(PhoneModel::Iphone16ProMax) => 0.155,
+            Some(PhoneModel::Iphone17ProMax) => 0.15,
             _ => 0.145,
         };
         (phone.width as f32 * ratio).round() as u32
